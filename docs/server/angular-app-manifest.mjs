901d645cc34f80873df5c4ebdd8df76d67fe9edf
PavithraBoyina2025 <boyinapavithra@gmail.com>
2025-02-14 20:27:08 +0530

    Update master branch with latest changes

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -30,7 +30,6 @@ export default {
     'home/index.html': {size: 18686, hash: '3b0f41d1660d43fa3773c38849056a0b6738aba5cd1e1fd9c0b1f07feb466d7a', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
     'menu/index.html': {size: 27125, hash: '86afe26c1dbf4c8e587a3e0e82ef8cd6969e95be27f810129aea828e287962ee', text: () => import('./assets-chunks/menu_index_html.mjs').then(m => m.default)},
     'booking/index.html': {size: 19622, hash: '2edd7fa73d3c50bd1449d6afd3053dcc31c41afdeedf91f0abb03ddedf70b2d9', text: () => import('./assets-chunks/booking_index_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 19660, hash: 'bcda265cb7675cd54ffe01eca426024a852d0e41486647e6e6e5a8de0081c9b0', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
     'styles-IG45FNYW.css': {size: 231634, hash: 'I3wd5rAhVIs', text: () => import('./assets-chunks/styles-IG45FNYW_css.mjs').then(m => m.default)}
   },
 };
